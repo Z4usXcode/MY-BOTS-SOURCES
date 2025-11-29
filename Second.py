298f4e993c48a21c0f0f78d0bf38d5d6f3928e98
Z4usXcode <alikytn55@gmail.com>
2025-11-29 14:27:34 +0300

    Update Second.py

--- Second.py
+++ Second.py
@@ -4,7 +4,7 @@ from datetime import datetime
 import sys
 
 # Bitiş tarihini buradan değiştir
-BITIS_TARIHI = datetime(2025, 12, 04, 23, 59, 0)
+BITIS_TARIHI = datetime(2025, 12, 4, 23, 59, 0)
 
 # Modül import edildiğinde otomatik kontrol
 if datetime.now() >= BITIS_TARIHI:
