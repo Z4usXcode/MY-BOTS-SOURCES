3b0cdb1eed6f77208e43a70d2d0495473ef5877f
Z4usXcode <alikytn55@gmail.com>
2025-11-29 14:26:14 +0300

    Create Second.py

---
+++ Second.py
@@ -0,0 +1,12 @@
+
+
+from datetime import datetime
+import sys
+
+# Bitiş tarihini buradan değiştir
+BITIS_TARIHI = datetime(2025, 12, 04, 23, 59, 0)
+
+# Modül import edildiğinde otomatik kontrol
+if datetime.now() >= BITIS_TARIHI:
+    print("⏳ API durduruldu! Satın alım için @Z4usXcode")
+    sys.exit(0)
